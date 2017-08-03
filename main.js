5abdf883560a802bf5c05132771ef20d92c4988f
Your Name <you@example.com>
2017-08-03 15:59:26 -0400

    audio not working

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 let searchTerm= document.querySelector("#inputValue");
 let button=document.querySelector("#musicButton");
 let main = document.querySelector(".container");
-let audio = document.querySelector(".header")
+let audio = document.querySelector(".header-music");
 button.addEventListener("click", fetchData);
+let play= document.querySelector("#playMusic");
+let playButton = document.querySelector(".playsong");
+let body = document.querySelector('.music-site');
+
 
 function fetchData(){
-  // fetch("http://crossorigin.me/http://www.recipepuppy.com/api/?q="+searchTerm.value)
   fetch("https://itunes.apple.com/search?term="+searchTerm.value)
-  // https://itunes.apple.com/search?parameterkeyvalue
   .then( function(response){
     if(response.status !== 200){
       console.log(response.status);
       return;
     }
       response.json().then(function(reponse){
-console.log(reponse.results);
+        console.log(reponse.results);
 
         reponse.results.forEach(function(result){
           main.innerHTML+=`
           <div class="music-box">
-
-
             <a href="#"><img src="${result.artworkUrl100}" alt=""></a>
             <div>
-            <a href= "${result.previewUrl}"><div>${result.trackName}</div></a>
+            <a href="#" class="playSong" value= "${result.previewUrl}"><div>${result.trackName}</div></a>
             <h3>${result.artistName}</h3>
-            <audio controls id=playMusic  src="${result.previewUrl}">
+            <audio controls id="playMusic"  src="${result.previewUrl}">
 
             </audio>
             </div>
@@ -34,21 +34,17 @@ console.log(reponse.results);
 
         `;
 
-
-        console.log(main.innerHTML);
       })
-
-body.addEventListener(“click”, function playMusic(e) {
-    if(e.target && e.target.matches(“img”)){
-        console.log(“you pressed a button”, e.target);
-        audioSource.src = e.target.getAttribute(‘data-music’);
-        audioSource.load();
-        audioSource.play();
-    }
-});
-
-
     })
 
   })
 }
+body.addEventListener("click", function playMusic(e) {
+    if(e.target && e.target.matches("a.playsong")){
+        console.log("you pressed a button", e.target);
+        audio.src = e.target.getAttribute("value");
+        // audio.appendchild(play);
+        audio.load();
+        audio.play();
+    }
+});
