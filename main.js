d74830259a3cce9662f3101d3c465c672c66bf1c
Your Name <you@example.com>
2017-08-04 14:47:18 -0400

    funtioning audio player

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let searchTerm= document.querySelector("#inputValue");
 let button=document.querySelector("#musicButton");
 let main = document.querySelector(".container");
 let audio = document.querySelector(".header-music");
-button.addEventListener("click", fetchData);
 let play= document.querySelector("#playMusic");
 let playButton = document.querySelector(".playsong");
 let body = document.querySelector('.music-site');
 
+body.addEventListener("click", function(e) {
 
-function fetchData(){
+if(e.target=== button){
   fetch("https://itunes.apple.com/search?term="+searchTerm.value)
   .then( function(response){
     if(response.status !== 200){
@@ -16,35 +16,24 @@ function fetchData(){
       return;
     }
       response.json().then(function(reponse){
-        console.log(reponse.results);
-
         reponse.results.forEach(function(result){
           main.innerHTML+=`
           <div class="music-box">
             <a href="#"><img src="${result.artworkUrl100}" alt=""></a>
             <div>
-            <a href="#" class="playSong" value= "${result.previewUrl}"><div>${result.trackName}</div></a>
-            <h3>${result.artistName}</h3>
-            <audio controls id="playMusic"  src="${result.previewUrl}">
-
-            </audio>
+              <a href="#" class="playSong" value= "${result.previewUrl}">${result.trackName}</a>
+              <h3>${result.artistName}</h3>
             </div>
-
           </div>
-
         `;
-
       })
     })
-
   })
 }
-body.addEventListener("click", function playMusic(e) {
-    if(e.target && e.target.matches("a.playsong")){
-        console.log("you pressed a button", e.target);
-        audio.src = e.target.getAttribute("value");
-        // audio.appendchild(play);
-        audio.load();
-        audio.play();
-    }
+  if(e.target && e.target.matches("a.playSong")){
+      console.log("you pressed a button", e.target);
+      audio.src = e.target.getAttribute("value");
+      audio.load();
+      audio.play();
+  }else{console.log(e.target)}
 });
